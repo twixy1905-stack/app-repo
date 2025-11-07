d63646f811f64e2d34bbceaa18ce383a4d05db1c
twixy1905-stack <twixy1905@gmail.com>
2025-11-07 12:17:31 +0100

    Initialize Flask app with a root endpoint

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.get("/")
+def root():
+    return jsonify(message="Hello from GKE via GitOps!")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
