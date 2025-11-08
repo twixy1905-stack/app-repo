1cd4743cb0f07e981bc004b75a9a26b6ae205696
twixy1905-stack <twixy1905@gmail.com>
2025-11-08 11:02:47 +0100

    Update message in root endpoint response

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def root():
-    return jsonify(message="Hello Team GKE!")
+    return jsonify(message="Hello from Cloud Build CI/CD GitOps!")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
