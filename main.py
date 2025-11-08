5e77c83e1d5c60d651405b3226696814c13dcb7c
twixy1905-stack <twixy1905@gmail.com>
2025-11-08 16:10:24 +0100

    Update root message in main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def root():
-    return jsonify(message="Hello from Cloud Build CI/CD GitOps!")
+    return jsonify(message="Hi from Cloud Build CI/CD GitOps! It was hard way to finish this project, but we made it!")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
