273400ea408300ee708774a9ad56d12c5508a1a2
twixy1905-stack <twixy1905@gmail.com>
2025-11-08 09:29:49 +0100

    Change greeting message in root endpoint

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def root():
-    return jsonify(message="Hello from GKE via GitOps!")
+    return jsonify(message="Hello Team GKE!")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
